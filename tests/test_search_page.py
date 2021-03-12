720310d7a989e9b523f60c823ec7503e47411921
VictorKazankov <sadovnichi@mail.ru>
2021-03-12 22:09:06 +0200

    Splitting tests into files by functionality ############ 1. Create test_search_page file 2. Add tests for search page to test_search_page file

---
+++ tests/test_search_page.py
@@ -0,0 +1,34 @@
+from pages.main_page import MainPage
+from pages.search_page import SearchPage
+
+main_page_url = "https://www.booking.com/"
+
+
+def test_booking_price_by_booking_date(browser):
+    main_page = MainPage(browser, main_page_url)
+    main_page.open()
+    main_page.open_bukovel_hotel_list()
+    search_page = SearchPage(browser, browser.current_url)
+    search_page.should_be_bukovel_result_page()
+    search_page.should_be_visible_booking_calendar()
+    search_page.should_not_be_booking_price_and_booking_status()
+
+
+def test_visibly_calendar_after_click_show_price_button(browser):
+    main_page = MainPage(browser, main_page_url)
+    main_page.open()
+    main_page.open_bukovel_hotel_list()
+    search_page = SearchPage(browser, browser.current_url)
+    search_page.click_price_button()
+    search_page.should_be_visible_booking_calendar()
+
+
+def test_present_hotel_price_after_run_searching(browser):
+    main_page = MainPage(browser, main_page_url)
+    main_page.open()
+    main_page.open_bukovel_hotel_list()
+    search_page = SearchPage(browser, browser.current_url)
+    search_page.click_price_button()
+    search_page.choose_booking_data()
+    search_page.click_search_button()
+    search_page.is_prices_for_every_result_items()
